3d35c68027e56fb3b293bd596350a16c627fe096
Marcin Adamiak <59664314+martingx89@users.noreply.github.com>
2023-09-07 10:45:27 +0000

    Improve: add routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/features/Navbar';
+import Home from './components/pages/Home';
+import BlankPage from './components/pages/BlankPage';
 
 const App =()=> {
-  return 
+  return (
+    <div className='app'>
+      <Router>
+        <Navbar/>
+        <Switch>
+          <Route path='/' exact component={Home}/>
+          <Route path='/blank' exact component={BlankPage}/>
+        </Switch>
+      </Router>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/components/pages/BlankPage.js
@@ -0,0 +1,7 @@
+const BlankPage = () => {
+    return <>
+    <h1>Blank Page</h1>
+    </>
+}
+
+export default BlankPage;
