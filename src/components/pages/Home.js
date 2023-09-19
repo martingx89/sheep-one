850a878c46a1323075e6f1f748e99f95b3ce1601
Marcin Adamiak <marcin_89@outlook.com>
2023-09-19 18:46:22 +0200

    Refactor: remove code

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,4 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { getAllData } from '../../redux/dataRedux';
-
 const Home = () => {
-  const data = useSelector(getAllData);
-  const dispatch = useDispatch();
-
   return (
     <>
       <h1>Home Page</h1>
--- src/redux/dataRedux.js
+++ src/redux/dataRedux.js
@@ -6,7 +6,6 @@ export const getAllData = (state) => state.data;
 //actions
 const createActionName = (actionName) => `app/data/${actionName}`;
 export const GET_DATA = createActionName('GET_DATA');
-console.log(GET_DATA);
 
 //action creators
 export const getData = (payload) => ({ type: GET_DATA, payload });
