bda2e13e427ceb6e73c74c0773c0a2c78c4bbdb5
Marcin Adamiak <marcin_89@outlook.com>
2023-09-19 16:34:01 +0200

    Add: new pages

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import './App.css';
 import React from 'react';
 import { Route, Routes } from 'react-router';
 import Home from './components/pages/Home';
+import News from './components/pages/News';
+import Catalog from './components/pages/Catalog';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
 import BlankPage from './components/pages/BlankPage';
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />}></Route>
+      <Route path='/news' element={<Home />}></Route>
+      <Route path='/catalog' element={<Home />}></Route>
+      <Route path='/about' element={<Home />}></Route>
+      <Route path='/contact' element={<Home />}></Route>
       <Route path='*' element={<BlankPage />}></Route>
     </Routes>
   );
