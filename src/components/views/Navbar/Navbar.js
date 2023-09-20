16e836ee9db191d0584702b3c8f0c2e53d236e66
Marcin Adamiak <marcin_89@outlook.com>
2023-09-20 19:25:14 +0200

    Add: link to home page

--- src/components/views/Navbar/Navbar.js
+++ src/components/views/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav>
+      <Link to='/'>Website</Link>
       <ul>
         <li>
           <Link to='/news'>News</Link>
