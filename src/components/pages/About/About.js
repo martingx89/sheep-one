47378f11560e9e9ed04e3f86c8d8182dedf60067
Marcin Adamiak <marcin_89@outlook.com>
2023-09-20 20:24:01 +0200

    Add: styles and sample text

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -2,6 +2,55 @@ const About = () => {
   return (
     <>
       <h1>About Page</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic dicta maxime, ab sint repellendus amet nesciunt
+        eligendi perferendis iure sunt fuga repellat magni quibusdam alias dolore velit! Excepturi, maiores ex adipisci
+        dolores, debitis soluta aliquid accusantium tempora sint magnam est architecto voluptatem odit veritatis saepe
+        aspernatur quas praesentium ipsam iure repellendus animi porro sequi reprehenderit? Quia earum nam excepturi
+        molestiae quos quis itaque eum harum recusandae assumenda, modi quibusdam delectus? Quibusdam laboriosam
+        mollitia, quos ducimus sunt ex sapiente recusandae atque culpa asperiores dolorum ut voluptate nemo. Eaque,
+        atque facilis labore inventore temporibus velit, voluptatem commodi vitae aspernatur fugit, maiores quia?
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic dicta maxime, ab sint repellendus amet nesciunt
+        eligendi perferendis iure sunt fuga repellat magni quibusdam alias dolore velit! Excepturi, maiores ex adipisci
+        dolores, debitis soluta aliquid accusantium tempora sint magnam est architecto voluptatem odit veritatis saepe
+        aspernatur quas praesentium ipsam iure repellendus animi porro sequi reprehenderit? Quia earum nam excepturi
+        molestiae quos quis itaque eum harum recusandae assumenda, modi quibusdam delectus? Quibusdam laboriosam
+        mollitia, quos ducimus sunt ex sapiente recusandae atque culpa asperiores dolorum ut voluptate nemo. Eaque,
+        atque facilis labore inventore temporibus velit, voluptatem commodi vitae aspernatur fugit, maiores quia?
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic dicta maxime, ab sint repellendus amet nesciunt
+        eligendi perferendis iure sunt fuga repellat magni quibusdam alias dolore velit! Excepturi, maiores ex adipisci
+        dolores, debitis soluta aliquid accusantium tempora sint magnam est architecto voluptatem odit veritatis saepe
+        aspernatur quas praesentium ipsam iure repellendus animi porro sequi reprehenderit? Quia earum nam excepturi
+        molestiae quos quis itaque eum harum recusandae assumenda, modi quibusdam delectus? Quibusdam laboriosam
+        mollitia, quos ducimus sunt ex sapiente recusandae atque culpa asperiores dolorum ut voluptate nemo. Eaque,
+        atque facilis labore inventore temporibus velit, voluptatem commodi vitae aspernatur fugit, maiores quia?
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic dicta maxime, ab sint repellendus amet nesciunt
+        eligendi perferendis iure sunt fuga repellat magni quibusdam alias dolore velit! Excepturi, maiores ex adipisci
+        dolores, debitis soluta aliquid accusantium tempora sint magnam est architecto voluptatem odit veritatis saepe
+        aspernatur quas praesentium ipsam iure repellendus animi porro sequi reprehenderit? Quia earum nam excepturi
+        molestiae quos quis itaque eum harum recusandae assumenda, modi quibusdam delectus? Quibusdam laboriosam
+        mollitia, quos ducimus sunt ex sapiente recusandae atque culpa asperiores dolorum ut voluptate nemo. Eaque,
+        atque facilis labore inventore temporibus velit, voluptatem commodi vitae aspernatur fugit, maiores quia?
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic dicta maxime, ab sint repellendus amet nesciunt
+        eligendi perferendis iure sunt fuga repellat magni quibusdam alias dolore velit! Excepturi, maiores ex adipisci
+        dolores, debitis soluta aliquid accusantium tempora sint magnam est architecto voluptatem odit veritatis saepe
+        aspernatur quas praesentium ipsam iure repellendus animi porro sequi reprehenderit? Quia earum nam excepturi
+        molestiae quos quis itaque eum harum recusandae assumenda, modi quibusdam delectus? Quibusdam laboriosam
+        mollitia, quos ducimus sunt ex sapiente recusandae atque culpa asperiores dolorum ut voluptate nemo. Eaque,
+        atque facilis labore inventore temporibus velit, voluptatem commodi vitae aspernatur fugit, maiores quia?
+      </p>
     </>
   );
 };
