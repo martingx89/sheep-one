650348db726ec3ba18a6b4615371c4920144a4ec
Marcin Adamiak <marcin_89@outlook.com>
2023-09-20 19:23:23 +0200

    Add: basic navigation

--- src/components/views/Navbar/Navbar.js
+++ src/components/views/Navbar/Navbar.js
@@ -1,5 +1,24 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
-  return <></>;
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to='/news'>News</Link>
+        </li>
+        <li>
+          <Link to='/catalog'>Catalog</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+        <li>
+          <Link to='contact'>Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
