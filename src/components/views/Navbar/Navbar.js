466922329ee4223071a4917ddcbd8a6a685b5d7c
Marcin Adamiak <marcin_89@outlook.com>
2023-09-21 19:58:51 +0200

    Add: fix styles

--- src/components/views/Navbar/Navbar.js
+++ src/components/views/Navbar/Navbar.js
@@ -1,38 +1,51 @@
-import { Link, NavLink } from 'react-router-dom';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { FiMenu, FiX } from 'react-icons/fi';
+import { Link, NavLink } from 'react-router-dom';
 import styles from './Navbar.module.scss';
+// import './Navbar.css';
 
 const Navbar = () => {
-
   const [open, setOpen] = useState(false);
 
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  const closeMenu = () => {
+    setOpen(false);
+  };
+
   return (
-    <>
-      <nav>
-        <div onClick={() => setOpen(!open)} className={styles['nav-icon']}>
-          {open ? <FiX /> : <FiMenu />}
-        </div>
-        <Link to='/' className={styles.title} onClick={() => setOpen(false)}>
+    <nav className={styles.nav}>
+      <Link to='/' className={styles.title}>
         Website
-        </Link>
-        <ul className={open ? `${styles['nav-links']} active` : styles['nav-links']}>
-          <li className={styles['nav-item']}>
-            <NavLink to='/news' onClick={() => setOpen(false)}>News</NavLink>
-          </li>
-          <li className={styles['nav-item']}>
-            <NavLink to='/catalog' onClick={() => setOpen(false)}>Catalog</NavLink>
-          </li>
-          <li className={styles['nav-item']}>
-            <NavLink to='/about' onClick={() => setOpen(false)}>About</NavLink>
-          </li>
-          <li className={styles['nav-item']}>
-            <NavLink to='contact' onClick={() => setOpen(false)}>Contact</NavLink>
-          </li>
-        </ul>
+      </Link>
+      <div onClick={handleClick} className='nav-icon'>
+        {open ? <FiX /> : <FiMenu />}
+      </div>
+      <ul className={open ? `${styles['nav-links']} active` : `${styles['nav-links']}`}>
+        <li className={`${styles['nav-item']}`}>
+          <NavLink to='/' className={`${styles['nav-link']}`} onClick={closeMenu}>
+            Home
+          </NavLink>
+        </li>
+        <li className='nav-item'>
+          <NavLink to='/about' className={`${styles['nav-link']}`} onClick={closeMenu}>
+            About
+          </NavLink>
+        </li>
+        <li className='nav-item'>
+          <NavLink to='/news' className={`${styles['nav-link']}`} onClick={closeMenu}>
+            Shop
+          </NavLink>
+        </li>
+        <li className='nav-item'>
+          <NavLink to='/contact' className={`${styles['nav-link']}`} onClick={closeMenu}>
+            Contact
+          </NavLink>
+        </li>
+      </ul>
     </nav>
-    </>
-    
   );
 };
 
