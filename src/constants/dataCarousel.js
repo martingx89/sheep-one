220b4e086ea55eb8b417764b3e1d50548ade9901
Marcin Adamiak <marcin_89@outlook.com>
2023-09-29 17:24:40 +0200

    Refactor: move temp data and add package

---
+++ src/constants/dataCarousel.js
@@ -0,0 +1,54 @@
+// Definicja danych kafelków
+
+export const dataCarousel = [
+  {
+    id: 1,
+    imageUrl:
+      'https://pixabay.com/get/gf42532286d870bc7379756a552ca86a6e30c7cd87253401f8c359e33157e2bb117117d8342321f888b29bebf785d3fc6eb7edbbcb65334e01c40605080526f39b2fc8e583853bc94448333b3cbe42689_640.jpg',
+    title: 'Tytuł 1',
+    description: 'Opis 1',
+  },
+  {
+    id: 2,
+    imageUrl:
+      'https://pixabay.com/get/gf42532286d870bc7379756a552ca86a6e30c7cd87253401f8c359e33157e2bb117117d8342321f888b29bebf785d3fc6eb7edbbcb65334e01c40605080526f39b2fc8e583853bc94448333b3cbe42689_640.jpg',
+    title: 'Tytuł 2',
+    description: 'Opis 2',
+  },
+  {
+    id: 3,
+    imageUrl:
+      'https://pixabay.com/get/gf42532286d870bc7379756a552ca86a6e30c7cd87253401f8c359e33157e2bb117117d8342321f888b29bebf785d3fc6eb7edbbcb65334e01c40605080526f39b2fc8e583853bc94448333b3cbe42689_640.jpg',
+    title: 'Tytuł 3',
+    description: 'Opis 3',
+  },
+  {
+    id: 4,
+    imageUrl:
+      'https://pixabay.com/get/gf42532286d870bc7379756a552ca86a6e30c7cd87253401f8c359e33157e2bb117117d8342321f888b29bebf785d3fc6eb7edbbcb65334e01c40605080526f39b2fc8e583853bc94448333b3cbe42689_640.jpg',
+    title: 'Tytuł 4',
+    description: 'Opis 4',
+  },
+  {
+    id: 5,
+    imageUrl:
+      'https://pixabay.com/get/gf42532286d870bc7379756a552ca86a6e30c7cd87253401f8c359e33157e2bb117117d8342321f888b29bebf785d3fc6eb7edbbcb65334e01c40605080526f39b2fc8e583853bc94448333b3cbe42689_640.jpg',
+    title: 'Tytuł 5',
+    description: 'Opis 5',
+  },
+  {
+    id: 6,
+    imageUrl:
+      'https://pixabay.com/get/gf42532286d870bc7379756a552ca86a6e30c7cd87253401f8c359e33157e2bb117117d8342321f888b29bebf785d3fc6eb7edbbcb65334e01c40605080526f39b2fc8e583853bc94448333b3cbe42689_640.jpg',
+    title: 'Tytuł 6',
+    description: 'Opis 6',
+  },
+  {
+    id: 7,
+    imageUrl:
+      'https://pixabay.com/get/gf42532286d870bc7379756a552ca86a6e30c7cd87253401f8c359e33157e2bb117117d8342321f888b29bebf785d3fc6eb7edbbcb65334e01c40605080526f39b2fc8e583853bc94448333b3cbe42689_640.jpg',
+    title: 'Tytuł 7',
+    description: 'Opis 7',
+  },
+  // Dodaj więcej danych kafelków według potrzeb
+];
