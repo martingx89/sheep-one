c52a64160349449251666f6d1b844b9bd7577fbe
Marcin Adamiak <marcin_89@outlook.com>
2023-09-30 13:08:19 +0200

    Add: sample data

--- src/components/views/Card/Card.js
+++ src/components/views/Card/Card.js
@@ -4,8 +4,12 @@ import styles from './Card.module.scss';
 function Card() {
   return (
     <div className={styles.wrapper}>
-      <img src='map.jpg' alt='map' />
-      <h2>Sample data</h2>
+      <img src={`${process.env.PUBLIC_URL}/assets/images/map_1/LSK-track_01.png`} alt='map' />
+      <div>
+        <h2>Źródełka</h2>
+        <p>Leskie źródełka</p>
+        <a href='https://pl.wikipedia.org/wiki/Owca_domowa'>Link</a>
+      </div>
     </div>
   );
 }
