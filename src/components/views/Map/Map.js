324f8115f2f5eb3d97d3eef704efb11636a0eb0e
Marcin Adamiak <marcin_89@outlook.com>
2023-10-01 22:14:51 +0200

    Add: get gps data from device

--- src/components/views/Map/Map.js
+++ src/components/views/Map/Map.js
@@ -1,21 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import styles from './Map.module.scss';
 
 const Map = () => {
-  const position = [49.4701, 22.33042]; // [latitude, longitude]
+  const [userPosition, setUserPosition] = useState(null);
   const zoomLevel = 13;
+
+  useEffect(() => {
+    // Function to get the user's current position using the Geolocation API
+    const getUserLocation = () => {
+      if ('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const { latitude, longitude } = position.coords;
+            setUserPosition([latitude, longitude]);
+          },
+          (error) => {
+            console.error('Error getting user location:', error);
+          }
+        );
+      } else {
+        console.error('Geolocation is not available in this browser.');
+      }
+    };
+
+    // Call the function to get the user's location
+    getUserLocation();
+  }, []);
 
   return (
     <div className={styles['map-wrapper']}>
-      <MapContainer center={position} zoom={zoomLevel} scrollWheelZoom={false}>
+      <MapContainer center={userPosition || [49.4701, 22.33042]} zoom={zoomLevel} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         />
-        <Marker position={position}>
-          <Popup>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, eum!</Popup>
-        </Marker>
+        {userPosition && (
+          <Marker position={userPosition}>
+            <Popup>Your current location</Popup>
+          </Marker>
+        )}
       </MapContainer>
     </div>
   );
