81195d2bdc3c299cd4697978afe37bc42ff1dff4
Marcin Adamiak <marcin_89@outlook.com>
2023-10-08 22:10:40 +0200

    Add: button inside map view

--- src/components/views/Map/Map.js
+++ src/components/views/Map/Map.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import ReactDOM from 'react-dom';
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchGPSData } from '../../../redux/userPositionRedux';
 import { INITIAL_COORDS } from '../../../constants/initialData';
 import LocateButton from '../../common/LocateButton/LocateButton';
 import styles from './Map.module.scss';
+import L from 'leaflet';
 
 const Map = () => {
   const dispatch = useDispatch();
@@ -20,6 +22,35 @@ const Map = () => {
     setMapKey(mapKey + 1);
   };
 
+  const CustomButtonControl = () => {
+    const map = useMap();
+
+    useEffect(() => {
+      if (!map) return;
+
+      const buttonControl = L.control({
+        position: 'bottomright',
+      });
+
+      buttonControl.onAdd = () => {
+        buttonControl._div = L.DomUtil.create('div', 'myControl');
+
+        const customButton = <LocateButton onClick={refreshMap}>Find me</LocateButton>;
+        ReactDOM.render(customButton, buttonControl._div);
+
+        return buttonControl._div;
+      };
+
+      buttonControl.addTo(map);
+
+      return () => {
+        map.removeControl(buttonControl);
+      };
+    }, [map]);
+
+    return null;
+  };
+
   return (
     <div className={styles['map-wrapper']}>
       <MapContainer key={mapKey} center={userPosition || INITIAL_COORDS} zoom={zoomLevel} scrollWheelZoom={true}>
@@ -27,15 +58,13 @@ const Map = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         />
-        {userPosition && (
-          <Marker position={userPosition}>
-            <Popup>Your current location</Popup>
-          </Marker>
-        )}
+        <Marker position={userPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+        <CustomButtonControl />
       </MapContainer>
-      <div className={styles['map-controls']}>
-        <LocateButton onClick={refreshMap}>Where I'm</LocateButton>
-      </div>
     </div>
   );
 };
