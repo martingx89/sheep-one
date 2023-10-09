603e0ee48ab89dcc7bf5e2191d98a86668b494c2
Marcin Adamiak <marcin_89@outlook.com>
2023-10-09 16:21:34 +0200

    Fix: user position update

--- src/components/views/Map/Map.js
+++ src/components/views/Map/Map.js
@@ -1,19 +1,13 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchGPSData } from '../../../redux/userPositionRedux';
+import { useSelector } from 'react-redux';
 import FindUserPosition from '../../../utils/mapUtils';
 import { INITIAL_COORDS } from '../../../constants/initialData';
 import { ZOOM_LEVEL } from '../../../constants/mapSettings';
 import styles from './Map.module.scss';
 
 const Map = () => {
-  const dispatch = useDispatch();
   const userPosition = useSelector((state) => state.userPosition);
-
-  useEffect(() => {
-    dispatch(fetchGPSData());
-  }, [dispatch]);
 
   return (
     <div className={styles['map-wrapper']}>
--- src/utils/mapUtils.js
+++ src/utils/mapUtils.js
@@ -1,17 +1,26 @@
 import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { useMap } from 'react-leaflet';
 import L from 'leaflet';
 import { MdGpsFixed } from 'react-icons/md';
+import { fetchGPSData } from '../redux/userPositionRedux';
 import LocateButton from '../components/common/LocateButton/LocateButton';
 import { createRoot } from 'react-dom/client';
 import { FLY_TO_SETUP } from '../constants/mapSettings';
 
 const FindUserPosition = ({ userPosition, ZOOM_LEVEL }) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchGPSData());
+  }, [dispatch]);
+
   const map = useMap();
 
   useEffect(() => {
     const refreshMap = () => {
       map.flyTo([userPosition[0], userPosition[1]], ZOOM_LEVEL, FLY_TO_SETUP);
+      console.log(userPosition);
     };
 
     if (!map) return;
