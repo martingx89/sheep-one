e2d86138329bead56ba4a167c54f91e0c8d156d2
Marcin Adamiak <59664314+martingx89@users.noreply.github.com>
2023-10-12 11:51:25 +0000

    Add: splashscreen

---
+++ src/components/pages/SplashScreen/SplashScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PageTitle from '../../common/PageTitle/PageTitle';
+import styles from './SplashScreen.module.scss'
+
+const SplashScreen = () => {
+  return (
+    <PageTitle>
+      Loading...
+    </PageTitle>
+  );
+};
+
+export default SplashScreen;
--- src/index.js
+++ src/index.js
@@ -7,16 +7,31 @@ import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import './styles/normalize.scss';
 import './styles/global.scss';
+import SplashScreen from './components/pages/SplashScreen/SplashScreen'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
 root.render(
   <React.StrictMode>
     <HashRouter>
       <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <App />
-        </PersistGate>
+        <SplashScreen />
       </Provider>
     </HashRouter>
   </React.StrictMode>
 );
+
+setTimeout(() => {
+  root.render(
+    <React.StrictMode>
+      <HashRouter>
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <App />
+          </PersistGate>
+        </Provider>
+      </HashRouter>
+    </React.StrictMode>
+  );
+}, 3000);
