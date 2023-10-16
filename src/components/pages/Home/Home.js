58a0165c06ec95d12ed4fdfdfeba67c76f7e6e88
Marcin Adamiak <marcin_89@outlook.com>
2023-10-16 14:08:52 +0200

    Patch: change styles

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,12 +1,13 @@
 import PageTitle from '../../common/PageTitle/PageTitle';
 import Carousel from '../../views/Carousel/Carousel';
+import styles from './Home.module.scss';
 
 const Home = () => {
   return (
-    <>
+    <div className={styles['home-wrapper']}>
       <PageTitle>Home Page</PageTitle>
       <Carousel />
-    </>
+    </div>
   );
 };
 
