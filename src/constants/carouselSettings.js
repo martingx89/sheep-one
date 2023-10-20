21b7a4bedd6521002dbb5ea352d24723673a553e
Marcin Adamiak <marcin_89@outlook.com>
2023-10-20 18:36:25 +0200

    Patch: carousel setup update

--- src/constants/carouselSettings.js
+++ src/constants/carouselSettings.js
@@ -1,5 +1,3 @@
-import MagicSliderDots from 'react-magic-slider-dots';
-
 export const CAROUSEL_SET = {
   arrows: false,
   // dots: true,
@@ -9,9 +7,6 @@ export const CAROUSEL_SET = {
   autoplaySpeed: 5000,
   slidesToShow: 3,
   slidesToScroll: 1,
-  // appendDots: (dots) => {
-  //   return <MagicSliderDots dots={dots} numDotsToShow={4} dotWidth={30} />;
-  // },
   responsive: [
     {
       breakpoint: 768,
