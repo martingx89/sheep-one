34796224365ade9ea1b172680243e370acb1bccb
Marcin Adamiak <marcin_89@outlook.com>
2023-10-29 18:45:46 +0100

    Add: yup validate

--- src/components/pages/Contact/Contact.js
+++ src/components/pages/Contact/Contact.js
@@ -1,44 +1,36 @@
 import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { TextField, Button } from '@mui/material';
-import styles from './Contact.module.scss';
+import * as Yup from 'yup';
 
 const Contact = () => {
   const handleSubmit = (values) => {
+    // Obsłuż dane z formularza
     console.log(values);
   };
 
-  const validate = (values) => {
-    const errors = {};
-
-    if (!values.name) {
-      errors.name = 'Pole Imię jest wymagane';
-    }
-
-    if (!values.email) {
-      errors.email = 'Pole Email jest wymagane';
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-      errors.email = 'Nieprawidłowy adres email';
-    }
-
-    if (!values.message) {
-      errors.message = 'Pole Wiadomość jest wymagane';
-    }
-
-    return errors;
-  };
+  // Zdefiniuj schemat walidacji z użyciem Yup
+  const validationSchema = Yup.object().shape({
+    name: Yup.string().required('Pole Imię jest wymagane').max(15, 'Must be 15 characters or less'),
+    email: Yup.string().email('Nieprawidłowy adres email').required('Pole Email jest wymagane'),
+    message: Yup.string().required('Pole Wiadomość jest wymagane'),
+  });
 
   return (
-    <Formik initialValues={{ name: '', email: '', message: '' }} onSubmit={handleSubmit} validate={validate}>
+    <Formik
+      initialValues={{ name: '', email: '', message: '' }}
+      onSubmit={handleSubmit}
+      validationSchema={validationSchema} // Przypisz schemat walidacji
+    >
       <Form>
         <div>
           <Field as={TextField} id='name' name='name' label='Imię' variant='outlined' fullWidth />
-          <ErrorMessage name='name' component='div' className={styles.error} />
+          <ErrorMessage name='name' component='div' className='error' />
         </div>
 
         <div>
           <Field as={TextField} type='email' id='email' name='email' label='Email' variant='outlined' fullWidth />
-          <ErrorMessage name='email' component='div' className={styles.error} />
+          <ErrorMessage name='email' component='div' className='error' />
         </div>
 
         <div>
@@ -52,10 +44,10 @@ const Contact = () => {
             multiline
             rows={4}
           />
-          <ErrorMessage name='message' component='div' className={styles.error} />
+          <ErrorMessage name='message' component='div' className='error' />
         </div>
 
-        <Button type='submit' variant='contained' className={styles.button}>
+        <Button type='submit' variant='contained' color='primary'>
           Wyślij
         </Button>
       </Form>
