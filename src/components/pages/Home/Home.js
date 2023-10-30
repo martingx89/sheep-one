76310110c940559be0f5c2ed2b3856ae2d517b50
Marcin Adamiak <marcin_89@outlook.com>
2023-10-30 21:53:10 +0100

    Patch: styles and service id for email

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -8,10 +8,10 @@ const Home = () => {
     <Container>
       <PageTitle>Oferta</PageTitle>
       <Grid container spacing={2}>
-        <Grid item xs={12} md={6}>
+        <Grid item xs={12} md={6} lg={6} xl={6}>
           <Carousel />
         </Grid>
-        <Grid item xs={12} md={6}>
+        <Grid item xs={12} md={6} lg={6} xl={6}>
           <Typography>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos illum laudantium totam itaque mollitia
--- src/constants/pageSetup.js
+++ src/constants/pageSetup.js
@@ -32,7 +32,7 @@ export const CAROUSEL_SET = {
   speed: 1000,
   autoplay: true,
   autoplaySpeed: 5000,
-  slidesToShow: 3,
+  slidesToShow: 4,
   slidesToScroll: 1,
   responsive: [
     {
@@ -50,7 +50,7 @@ export const CAROUSEL_SET = {
   ],
 };
 export const EMAIL_API_KEY = 't6P4kn3Rqaa1sHP6E';
-export const EMAIL_SERVICE_ID = 'service_pu8lf7y';
+export const EMAIL_SERVICE_ID = 'service_vm39kzn';
 export const EMAIL_TEMPLATE_ID = 'template_jqp0u9o';
 export const PAGES = ['Home', 'Regulamin', 'Trasy', 'O nas', 'Kontakt'];
 export const PATHS = ['', 'statue', 'catalog', 'about', 'contact'];
