6e83044dce8728cceac266350146b8e3c2a2d777
Marcin Adamiak <59664314+martingx89@users.noreply.github.com>
2023-11-17 08:44:27 +0000

    Fix: nav buttons

--- src/components/views/Navbar/Navbar.js
+++ src/components/views/Navbar/Navbar.js
@@ -31,111 +31,117 @@ const Navbar = () => {
 
   return (
     <AppBar position="static">
-      <Container maxWidth="xl">
-        <AppToolbar disableGutters variant="dense">
-          <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{
-              mr: 2,
-              display: { xs: "none", md: "flex" },
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            <Logo to={"/"}>{PAGE_TITLE_LONG}</Logo>
-          </Typography>
+      <AppToolbar disableGutters variant="dense">
+        <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+        <Typography
+          variant="h6"
+          noWrap
+          component="div"
+          sx={{
+            mr: 2,
+            display: { xs: "none", md: "flex" },
+            fontFamily: "monospace",
+            fontWeight: 700,
+            letterSpacing: ".3rem",
+            color: "inherit",
+            textDecoration: "none",
+          }}
+        >
+          <Logo to={"/"}>{PAGE_TITLE_LONG}</Logo>
+        </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              <MenuItem>
-                <Typography>
-                  <NavLink to="/" onClick={handleCloseNavMenu}>
-                    Home
-                  </NavLink>
-                  <NavLink to="/rules" onClick={handleCloseNavMenu}>
-                    Regulamin
-                  </NavLink>
-                  <NavLink smooth to="/#contact" onClick={handleCloseNavMenu}>
-                    Kontakt
-                  </NavLink>
-                </Typography>
-              </MenuItem>
-            </Menu>
-          </Box>
-          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="div"
+        <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+          <IconButton
+            size="large"
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleOpenNavMenu}
+            color="inherit"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorElNav}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "left",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "left",
+            }}
+            open={Boolean(anchorElNav)}
+            onClose={handleCloseNavMenu}
             sx={{
-              mr: 2,
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
+              display: { xs: "block", md: "none" },
             }}
           >
-            <Logo to={"/"}>{PAGE_TITLE_SHORT}</Logo>
-          </Typography>
-          <Box
-            sx={{
-              flexGrow: 1,
-              display: { xs: "none", md: "flex" },
-              justifyContent: "flex-end",
-            }}
-          >
-            <Button>
-              <MenuItem>
+            <MenuItem>
+              <Typography>
                 <NavLink to="/" onClick={handleCloseNavMenu}>
                   Home
                 </NavLink>
                 <NavLink to="/rules" onClick={handleCloseNavMenu}>
                   Regulamin
                 </NavLink>
-                <NavItem smooth to="/#contact" onClick={handleCloseNavMenu}>
+                <NavLink smooth to="/#contact" onClick={handleCloseNavMenu}>
                   Kontakt
-                </NavItem>
-              </MenuItem>
-            </Button>
-          </Box>
-        </AppToolbar>
-      </Container>
+                </NavLink>
+              </Typography>
+            </MenuItem>
+          </Menu>
+        </Box>
+        <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+        <Typography
+          variant="h5"
+          noWrap
+          component="div"
+          sx={{
+            mr: 2,
+            display: { xs: "flex", md: "none" },
+            flexGrow: 1,
+            fontFamily: "monospace",
+            fontWeight: 700,
+            letterSpacing: ".3rem",
+            color: "inherit",
+            textDecoration: "none",
+          }}
+        >
+          <Logo to={"/"}>{PAGE_TITLE_SHORT}</Logo>
+        </Typography>
+        <Box
+          sx={{
+            flexGrow: 1,
+            display: { xs: "none", md: "flex" },
+            justifyContent: "flex-end",
+          }}
+        >
+          <Button>
+            <MenuItem>
+              <NavLink to="/" onClick={handleCloseNavMenu}>
+                Home
+              </NavLink>
+            </MenuItem>
+          </Button>
+          <Button>
+            <MenuItem>
+              <NavLink to="/rules" onClick={handleCloseNavMenu}>
+                Regulamin
+              </NavLink>
+            </MenuItem>
+          </Button>
+          <Button>
+            <MenuItem>
+              <NavItem smooth to="/#contact" onClick={handleCloseNavMenu}>
+                Kontakt
+              </NavItem>
+            </MenuItem>
+          </Button>
+        </Box>
+      </AppToolbar>
     </AppBar>
   );
 };
